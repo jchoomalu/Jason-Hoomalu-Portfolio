9636115050fd82cd45999f37b566b432fa9de5d8
Jason Hoomalu <jasonhoomalu@Jasons-Air.lan>
2023-11-15 08:16:00 -0700

    Main Nav and secondary Nav completed with paths using react router for main nav and state manipulation for secondary innerNav component.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,39 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, NavDropdown, Image, Container } from "react-bootstrap";
+import { useLocation } from "react-router";
 import logo from "../../assets/images/jason_hoomalu_logo_long2.png";
 import accent from "../../assets/images/accent.png";
 import "./Nav.css";
 
 function Navigation() {
+  const [activeAbout, setActiveAbout] = useState("About Me");
+  const [activeProjects, setActiveProjects] = useState("Projects");
+  const location = useLocation(); // Get the current location using useLocation from React Router
+
+  useEffect(() => {
+    // Update active page based on the current location pathname
+    switch (location.pathname) {
+      case "/":
+        setActiveAbout("About Me");
+        setActiveProjects("Projects");
+        break;
+      case "/education":
+        setActiveAbout("Education");
+        break;
+      case "/experience":
+        setActiveAbout("Experience");
+        break;
+      case "/contact":
+        setActiveAbout("Contact");
+        break;
+      case "/gotyoursix":
+        setActiveProjects("Got Your Six");
+        break;
+      case "/symbolsaga":
+        setActiveProjects("Symbol Saga");
+        break;
+      case "/ssmapeditor":
+        setActiveProjects("SS Map Editor");
+        break;
+      case "/testimonials":
+        setActiveProjects("Testimonials");
+        break;
+      default:
+        break;
+    }
+  }, [location.pathname]);
+
   return (
     <>
-      <Navbar className="navbar" variant="dark" expand="md">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Container className="d-flex justify-content-evenly align-items-center mb-5">
-            <NavDropdown className="pill-dropdown" title="About Me" id="aboutMeDropdown">
-              <NavDropdown.Item href="#action/3.3">About Me</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Education</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Experience</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Contact</NavDropdown.Item>
-            </NavDropdown>
-            <Navbar.Brand className="d-none d-md-block" href="#home">
-              <Image className="logo" src={logo} />
-            </Navbar.Brand>
-            <NavDropdown className="pill-dropdown" title="Projects" id="projectsDropdown">
-              <NavDropdown.Item href="#">Got Your Six</NavDropdown.Item>
-              <NavDropdown.Item href="#">Symbol Saga</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">SS Map Editor</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Testimonials</NavDropdown.Item>
-            </NavDropdown>
-          </Container>
-        </Navbar.Collapse>
-      </Navbar>
-        <Image className="accentbar" src={accent} />
-    </>
+    <Navbar className="main-nav" variant="dark" expand="md">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Container className="d-flex justify-content-evenly align-items-center mb-5">
+          <NavDropdown className="pill-dropdown" title={activeAbout} id="aboutMeDropdown">
+            <NavDropdown.Item href="/">About Me</NavDropdown.Item>
+            <NavDropdown.Item href="/education">Education</NavDropdown.Item>
+            <NavDropdown.Item href="/experience">Experience</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
+          </NavDropdown>
+          <Navbar.Brand className="d-none d-md-block" href="/">
+            <Image className="logo" src={logo} />
+          </Navbar.Brand>
+          <NavDropdown className="pill-dropdown" title={activeProjects} id="projectsDropdown">
+            <NavDropdown.Item href="/gotyoursix">Got Your Six</NavDropdown.Item>
+            <NavDropdown.Item href="/symbolsaga">Symbol Saga</NavDropdown.Item>
+            <NavDropdown.Item href="/ssmapeditor">SS Map Editor</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/testimonials">Testimonials</NavDropdown.Item>
+          </NavDropdown>
+        </Container>
+      </Navbar.Collapse>
+    </Navbar>
+      <Image className="accentbar" src={accent} />
+  </>
   );
 }
 
 export default Navigation;
+
+
