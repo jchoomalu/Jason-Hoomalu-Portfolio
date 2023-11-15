8f3280b4857526dd179073dbb1db39817e27457a
Jason Hoomalu <jasonhoomalu@Jasons-Air.lan>
2023-11-14 19:28:48 -0700

    nav bar styles and logo adjustment

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Navbar, NavDropdown, Image, Container } from "react-bootstrap";
-import logo from "../../assets/images/jason_hoomalu_logo_long.png";
+import logo from "../../assets/images/jason_hoomalu_logo_long2.png";
 import accent from "../../assets/images/accent.png";
 import "./Nav.css";
 
 function Navigation() {
   return (
     <>
-      <Navbar className="navbar" bg="light" variant="light" expand="md">
+      <Navbar className="navbar" variant="dark" expand="md">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Container className="d-flex justify-content-evenly align-items-center">
-            <NavDropdown title="About Me" id="basic-nav-dropdown">
+          <Container className="d-flex justify-content-evenly align-items-center mb-5">
+            <NavDropdown className="pill-dropdown" title="About Me" id="aboutMeDropdown">
               <NavDropdown.Item href="#action/3.3">About Me</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">Education</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Experience</NavDropdown.Item>
@@ -21,12 +21,12 @@ function Navigation() {
             <Navbar.Brand className="d-none d-md-block" href="#home">
               <Image className="logo" src={logo} />
             </Navbar.Brand>
-            <NavDropdown title="Projects" id="basic-nav-dropdown">
+            <NavDropdown className="pill-dropdown" title="Projects" id="projectsDropdown">
               <NavDropdown.Item href="#">Got Your Six</NavDropdown.Item>
               <NavDropdown.Item href="#">Symbol Saga</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">SS Map Editor</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Contact</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Testimonials</NavDropdown.Item>
             </NavDropdown>
           </Container>
         </Navbar.Collapse>
