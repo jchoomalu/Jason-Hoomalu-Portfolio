bcbe6dc3a7a12785b37e7bfc3b4eb0f842088667
Jason Hoomalu <jason.hoomalu@gmail.com>
2023-11-24 08:40:54 -0700

    form revert

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,65 +1,27 @@
-import React from "react";
-import { Container, Form, Row, Col } from "react-bootstrap";
-import "./Contact.css";
+import React from 'react';
+// import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import './Contact.css';
 
 const Contact = () => {
   return (
-    <Container>
-      <Form
-        name="contact"
-        className="text-white col-12 col-md-9 mx-auto"
-        netlify
-      >
-        <Form.Group controlId="name">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your name" name="name" />
-        </Form.Group>
-
-        <Row>
-          <Col md="6">
-            <Form.Group controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="email@example.com"
-                name="email"
-              />
-            </Form.Group>
-          </Col>
-
-          <Col md="6">
-            <Form.Group controlId="phone">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control
-                type="tel"
-                placeholder="Enter your phone number"
-                name="phone"
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-
-        <Form.Group controlId="message">
-          <Form.Label>Message</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={5}
-            placeholder="Enter a message..."
-            name="message"
-          />
-        </Form.Group>
-
-        <div className="text-center">
-          <button
-            variant="primary"
-            type="submit"
-            className="submit-btn my-3 w-75 p-2"
-          >
-            Send Message
-          </button>
-        </div>
-      </Form>
-    </Container>
+        <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="name">Name</label> <br />
+        <input type="text" id="name" name="name" required />
+      </p>
+      <p>
+        <label htmlFor="email">Email</label> <br />
+        <input type="email" id="email" name="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message</label> <br />
+        <textarea id="message" name="message" required></textarea>
+      </p>
+      <p>
+        <input type="submit" value="Submit message" />
+      </p>
+    </form>
   );
 };
 
