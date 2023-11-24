630473c950ab4062d6f43900e32405c78969eddc
Jason Hoomalu <jason.hoomalu@gmail.com>
2023-11-24 08:45:35 -0700

    form revert

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,27 +1,68 @@
-import React from 'react';
-// import { Container, Form, Row, Col, Button } from 'react-bootstrap';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-        <form name="contact" method="post">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label htmlFor="name">Name</label> <br />
-        <input type="text" id="name" name="name" required />
-      </p>
-      <p>
-        <label htmlFor="email">Email</label> <br />
-        <input type="email" id="email" name="email" required />
-      </p>
-      <p>
-        <label htmlFor="message">Message</label> <br />
-        <textarea id="message" name="message" required></textarea>
-      </p>
-      <p>
-        <input type="submit" value="Submit message" />
-      </p>
-    </form>
+    <div class="container">
+      <form
+        name="contact"
+        class="text-white col-12 col-md-9 mx-auto"
+        netlify="true"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <div class="form-group" id="name">
+          <label for="name">Name</label>
+          <input
+            class="form-control my-1"
+            name="name"
+            type="text"
+            placeholder="Enter your name"
+          />
+        </div>
+
+        <div class="row">
+          <div class="col my-1" md="6">
+            <div class="form-group" id="email">
+              <label for="email">Email address</label>
+              <input
+                class="form-control"
+                type="email"
+                name="email"
+                placeholder="email@example.com"
+              />
+            </div>
+          </div>
+
+          <div class="col my-1" md="6">
+            <div class="form-group" id="phone">
+              <label for="phone">Phone Number</label>
+              <input
+                class="form-control"
+                type="tel"
+                name="phone"
+                placeholder="Enter your phone number"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group my-1" id="message">
+          <label for="message">Message</label>
+          <textarea
+            class="form-control"
+            rows="5"
+            name="message"
+            placeholder="Enter a message..."
+          ></textarea>
+        </div>
+
+        <div class="text-center">
+          <button type="submit" className="submit-btn my-3 w-75 p-2">
+            Send Message
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
