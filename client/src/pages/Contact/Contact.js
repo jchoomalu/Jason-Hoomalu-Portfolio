80e40c6a0b6318ad4fd26810a48e29b26f1dc25a
Jason Hoomalu <jason.hoomalu@gmail.com>
2023-11-24 05:02:17 -0700

    update netlify form and center submit button

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -4,56 +4,65 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div class="container">
-    <form name="contact" class="text-white col-12 col-md-9 mx-auto" netlify>
-      <div class="form-group" id="name">
-        <label for="name">Name</label>
-        <input
-          class="form-control my-2"
-          name="name"
-          type="text"
-          placeholder="Enter your name"
-        />
-      </div>
-      <div class="row">
-        <div class="col my-2" md="6">
-          <div class="form-group" id="email">
-            <label for="email">Email address</label>
-            <input
-              class="form-control"
-              type="email"
-              name="email"
-              placeholder="email@example.com"
-            />
+      <form
+        name="contact"
+        class="text-white col-12 col-md-9 mx-auto"
+        data-netlify="true"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <div class="form-group" id="name">
+          <label for="name">Name</label>
+          <input
+            class="form-control my-2"
+            name="name"
+            type="text"
+            placeholder="Enter your name"
+          />
+        </div>
+
+        <div class="row">
+          <div class="col my-2" md="6">
+            <div class="form-group" id="email">
+              <label for="email">Email address</label>
+              <input
+                class="form-control"
+                type="email"
+                name="email"
+                placeholder="email@example.com"
+              />
+            </div>
+          </div>
+
+          <div class="col my-2" md="6">
+            <div class="form-group" id="phone">
+              <label for="phone">Phone Number</label>
+              <input
+                class="form-control"
+                type="tel"
+                name="phone"
+                placeholder="Enter your phone number"
+              />
+            </div>
           </div>
         </div>
-        <div class="col my-2" md="6">
-          <div class="form-group" id="phone">
-            <label for="phone">Phone Number</label>
-            <input
-              class="form-control"
-              type="tel"
-              name="phone"
-              placeholder="Enter your phone number"
-            />
-          </div>
+
+        <div class="form-group my-2" id="message">
+          <label for="message">Message</label>
+          <textarea
+            class="form-control"
+            rows="5"
+            name="message"
+            placeholder="Enter a message..."
+          ></textarea>
         </div>
-      </div>
-      <div class="form-group my-2" id="message">
-        <label for="message">Message</label>
-        <textarea
-          class="form-control"
-          rows="5"
-          name="message"
-          placeholder="Enter a message..."
-        ></textarea>
-      </div>
-      <div class="text-center">
-        <button type="submit" class="submit-btn my-3 w-75 p-2">
-          Send Message
-        </button>
-      </div>
-    </form>
-  </div>  
+
+        <div class="text-center">
+          <button type="submit" class="submit-btn my-3 w-75 p-2">
+            Send Message
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
